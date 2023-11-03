f64ceffe10524fc304fe5e2eccfd64068eb0068d
Prabhat Kumar <pkkrsingh877@gmail.com>
2023-11-03 06:41:29 +0530

    Working on Delete and Update Features in frontend side, also removed doller symbol and added rupees symbol

--- frontend/src/components/Expenses/Expenses.jsx
+++ frontend/src/components/Expenses/Expenses.jsx
@@ -1,11 +1,22 @@
 import { Link } from 'react-router-dom';
 import useFetch from '../Utilities/useFetch'
 import { useEffect, useState } from 'react';
+import  '../../css/expenses.css';
 
 const Expenses = () => {
     const [expenses, setExpenses] = useState([]);
     const { data, pending, error } = useFetch('http://localhost:8000/expenses');
-    // will complete this 100%
+    const deleteButton = document.querySelector('#deleteButton');
+    const updateButton = document.querySelector('#updateButton');
+
+    updateButton.addEventListener('click', (e) => {
+        // update operation code
+    });
+
+    deleteButton.addEventListener('click', (e) => {
+        // delete operation code
+    });
+
     useEffect(() => {
         if (data) {
             setExpenses(data);
@@ -16,9 +27,14 @@ const Expenses = () => {
     return (
         <div>
             <Link to={`/create`} >Add an Expense</Link>
-            <ul>
+            <ul className='list' type='none'>
                 {expenses && expenses.map(expense => (
-                    <li key={expense._id}> ${expense.totalAmount}</li>
+                    <li key={expense._id} className='list-items'> 
+                    <span>{expense.productName}</span> 
+                    <span>&#x20B9;{expense.totalAmount}</span>
+                    <button id="updateButton">Update</button>
+                    <button id="deleteButton">Delete</button>
+                    </li>
                 ))}
             </ul>
         </div>

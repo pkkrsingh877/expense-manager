6095b9b497e6d8c8883b3977a8a584178fd24394
Prabhat Kumar <pkkrsingh877@gmail.com>
2023-11-17 12:49:45 +0530

    Added Basic CSS to index.html file

--- frontend/src/components/Utilities/Navbar.jsx
+++ frontend/src/components/Utilities/Navbar.jsx
@@ -4,7 +4,7 @@ import '../../css/navbar.css';
 const Navbar = () => {
     return (
         <nav className='navbar'>
-            <h1 className='nav-items'>Expense Tracker</h1>
+            <h1 className='nav-items display-1'>Expense Tracker</h1>
             <div className="nav-items nav-links">
                 <Link to={'/expense'} className="nav-link">Home</Link>
                 <Link to={'/expense/create'} className="nav-link">Create</Link>

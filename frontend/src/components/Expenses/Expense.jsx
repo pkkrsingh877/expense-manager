05c45156c4ae226987072af8c717a9ae430aad06
Prabhat Kumar <pkkrsingh877@gmail.com>
2023-11-19 20:51:36 +0530

    Card instead of table with update and delete features and navigation after deletion

--- frontend/src/components/Expenses/Expense.jsx
+++ frontend/src/components/Expenses/Expense.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import useFetch from '../Utilities/useFetch';
 
 const Expense = () => {
     const { id } = useParams();
     const [expense, setExpense] = useState();
+    const navigate = useNavigate();
 
     const { data, pending, error } = useFetch(`http://localhost:8000/expense/${id}`);
 
@@ -14,6 +15,25 @@ const Expense = () => {
         }
     }, [data]);
 
+    const handleDelete = async (id) => {
+        // Add the expense to your backend here
+        const response = await fetch(`http://localhost:8000/expense`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ id })
+        });
+
+        const message = await response.json();
+
+        if (message.status === 'success') {
+            navigate('/expense')
+        } else if (message.status === 'failure') {
+            // Later handle it
+        }
+    }
+
     return (
         <div>
             {error && <div>Error: {error}</div>}
@@ -21,45 +41,24 @@ const Expense = () => {
 
             {expense && (
                 <>
-                <h2>Product Data:</h2>
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">Attributes</th>
-                                <th scope="col">Values</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row">Product Name</th>
-                                <td>{expense.productName}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Number Of Products</th>
-                                <td>{expense.numberOfProducts}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Total Amount</th>
-                                <td colSpan="2">{expense.totalAmount}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Notes</th>
-                                <td colSpan="2">{expense.notes}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Type Of Expense</th>
-                                <td colSpan="2">{expense.typeOfExpense}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Created At</th>
-                                <td colSpan="2">{expense.createdAt}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Updated At</th>
-                                <td colSpan="2">{expense.updatedAt}</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <h2>Product Data:</h2>
+                    <div className="card mb-3 shadow p-3 bg-body rounded" key={expense._id} id={expense._id}>
+                        <div className="card-body">
+                            <h5 className="card-title"><Link to={`/expense/${expense._id}`}>
+                                {expense.productName}
+                            </Link></h5>
+                            <p className="card-text"><b>Price:</b> &#x20B9;{expense.totalAmount}</p>
+                            <p className="card-text"><b>Last Updated At:</b> {new Date(expense.updatedAt).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true }) +
+                                ", " +
+                                new Date(expense.updatedAt).toLocaleDateString('en-US', { day: '2-digit', month: 'short', year: 'numeric' })}</p>
+                            <div className="buttons">
+                                <Link to={`/expense/update/${expense._id}`} className="updateButton btn btn-primary">
+                                    Update
+                                </Link>
+                                <button className="deleteButton  btn btn-danger" onClick={() => handleDelete(expense._id)}>Delete</button>
+                            </div>
+                        </div>
+                    </div>
                 </>)}
         </div>
     );
